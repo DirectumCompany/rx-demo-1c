6305c4616aaf6e73cf30a418a5c03eb139845c8c
Шевелев Антон <shevelev_aa@directum.ru>
2024-12-10 17:24:36 +0400

    Добавлен репозиторий для демонстрации работы с 1С

---
+++ src/Packages/Sungero.Integration1CDemo/Sungero.Integration1CDemo.Server/IncomingInvoice/IncomingInvoiceServerFunctions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Integration1CDemo.IncomingInvoice;
+using IncomingInvoiceConstants = Sungero.Integration1CDemo.PublicConstants.Contracts.IncomingInvoice;
+
+namespace Sungero.Integration1CDemo.Server
+{
+  partial class IncomingInvoiceFunctions
+  {
+
+  }
+}
