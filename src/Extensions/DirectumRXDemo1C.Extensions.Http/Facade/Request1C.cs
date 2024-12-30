ca509abdcc498984945878034d4531ed5236299a
Пестриков Олег (Pestrikov_OS) <pestrikov_os@directum.ru>
2024-12-30 21:21:34 +0300

    #391749: Рефакторинг решения rx-demo-1c (набросок Request1C)

---
+++ src/Extensions/DirectumRXDemo1C.Extensions.Http/Facade/Request1C.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using DirectumRXDemo1C.Extensions.Http.Internal;
+
+namespace DirectumRXDemo1C.Extensions.Http
+{
+  public class Request1C
+  {
+    private HttpMethod method;
+    private readonly string url;
+    private readonly object content;
+
+    internal Request1C(HttpMethod method, string url, object content)
+    {
+      this.method = method;
+      this.url = url;
+      this.content = content;
+    }
+
+    public void Execute()
+    {
+      var response = HttpClientLocator.GetOrCreate().SendAsync(this.CreateRequest()).Result;
+      response.EnsureSuccessStatusCode();
+
+      this.Result = response.Content.ReadAsStringAsync().Result;
+    }
+
+    private HttpRequestMessage CreateRequest()
+    {
+      var requestBuilder = new HttpRequestMessageBuilder(method, url);
+      if (HttpMethodHelper.MethodRequiresContent(method))
+        requestBuilder.AppendContent(content);
+
+      return requestBuilder.Result;
+    }
+
+    public string Result { get; private set; }
+  }
+}
---
+++ src/Extensions/DirectumRXDemo1C.Extensions.Http/Internal/HttpRequestMessageBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace DirectumRXDemo1C.Extensions.Http.Internal
+{
+  internal class HttpRequestMessageBuilder
+  {
+    public HttpRequestMessageBuilder(HttpMethod method, string url) =>
+      this.Result = new HttpRequestMessage(method, url);
+
+    public void AppendContent(object content)
+    {
+      if (content == null)
+        throw new ArgumentNullException(nameof(content));
+
+      this.Result.Content = new StringContent(JsonConvert.SerializeObject(content), Encoding.UTF8, "application/json");
+    }
+
+    public HttpRequestMessage Result { get; }
+  }
+}
