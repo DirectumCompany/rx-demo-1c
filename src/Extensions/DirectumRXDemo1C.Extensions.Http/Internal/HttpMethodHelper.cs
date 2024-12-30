5e59097da7c0822ba614bd635930b2d99113aa37
Пестриков Олег (Pestrikov_OS) <pestrikov_os@directum.ru>
2024-12-30 21:21:34 +0300

    #391749: Рефакторинг решения rx-demo-1c (набросок Request1C)

---
+++ src/Extensions/DirectumRXDemo1C.Extensions.Http/Internal/HttpMethodHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+
+namespace DirectumRXDemo1C.Extensions.Http.Internal
+{
+  internal static class HttpMethodHelper
+  {
+    public static string ConvertToString(HttpMethod method)
+      => method.Method.ToUpperInvariant();
+
+    public static bool MethodRequiresContent(HttpMethod method)
+      => method == HttpMethod.Post || ConvertToString(method) == "PATCH";
+  }
+}
