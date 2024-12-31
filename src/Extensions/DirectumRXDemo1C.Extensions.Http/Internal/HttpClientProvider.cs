4896876c60f0172cb1fec821bc5cfc9362988758
Пестриков Олег (Pestrikov_OS) <pestrikov_os@directum.ru>
2024-12-31 10:02:43 +0300

    #391749: Рефакторинг решения rx-demo-1c (завершено)

---
+++ src/Extensions/DirectumRXDemo1C.Extensions.Http/Internal/HttpClientProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DirectumRXDemo1C.Extensions.Http.Internal
+{
+  internal static class HttpClientProvider
+  {
+    public static HttpClient Get() => lazyInstance.Value;
+
+    #region lazy instance
+
+    private static Lazy<HttpClient> lazyInstance = new Lazy<HttpClient>(CreateClient);
+
+    private static HttpClient CreateClient()
+    {
+      var httpClientHandler = new HttpClientHandler
+      {
+        ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true
+      };
+      return new HttpClient(httpClientHandler)
+      {
+        Timeout = TimeSpan.FromMinutes(5)
+      };
+    }
+
+    #endregion
+  }
+}
