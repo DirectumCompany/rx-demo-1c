f88f31503e4eae9a230252c3c2e4ab0427e6f9ce
Ковалев Максим <kovalev_mk@directum.ru>
2025-01-13 08:49:41 +0400

    #371643 Добавлена кнопка "Открыть  в 1С"

--- src/Packages/Sungero.Demo1C/Sungero.Demo1C.ClientBase/UniversalTransferDocument/UniversalTransferDocumentActions.cs
+++ src/Packages/Sungero.Demo1C/Sungero.Demo1C.ClientBase/UniversalTransferDocument/UniversalTransferDocumentActions.cs
@@ -7,4 +7,20 @@ using Sungero.Demo1C.UniversalTransferDocument;
 
 namespace Sungero.Demo1C.Client
 {
+  partial class UniversalTransferDocumentActions
+  {
+    public virtual void OpenEntityIn1CSungero(Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      var warning = Sungero.Demo1C.PublicFunctions.Module.OpenEntityIn1CFor(_obj);
+      if (warning != null)
+        e.AddWarning(warning);
+    }
+
+    public virtual bool CanOpenEntityIn1CSungero(Sungero.Domain.Client.CanExecuteActionArgs e)
+    {
+      return true;
+    }
+
+  }
+
 }
