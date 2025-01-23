70978a8194ebd72468b3433516365dbeac0fa3a8
Шевелев Антон <shevelev_aa@directum.ru>
2025-01-23 19:12:17 +0400

    1С-Демо. Загрузка вариантов процессов на стенд

--- src/Packages/Sungero.NoCodeApproval/Sungero.NoCodeApproval.Server/ModuleInitializer.cs
+++ src/Packages/Sungero.NoCodeApproval/Sungero.NoCodeApproval.Server/ModuleInitializer.cs
@@ -9,5 +9,28 @@ namespace Sungero.NoCodeApproval.Server
 {
   public partial class ModuleInitializer
   {
+
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      CreateOutgoingInvoiceExternalLinkIfNotExist();
+    }
+    
+    /// <summary>
+    /// Создать внешнюю ссылку (ExternalLink) для исходящего счета, если она не существует.
+    /// </summary>
+    private void CreateOutgoingInvoiceExternalLinkIfNotExist()
+    {
+      InitializationLogger.Debug("Init: Create OutgoingInvoice document type external link");
+      var documentType = Docflow.PublicInitializationFunctions.Module.GetDocumentTypeByTypeGuid(Constants.Module.OutgoingInvoiceTypeGuid);
+      if (documentType == null)
+      {
+        Docflow.PublicInitializationFunctions.Module.CreateDocumentType(Sungero.Contracts.Resources.OutgoingInvoiceTypeName,
+                                                                        Constants.Module.OutgoingInvoiceTypeGuid,
+                                                                        Docflow.DocumentType.DocumentFlow.Outgoing,
+                                                                        Sungero.CoreEntities.DatabookEntry.Status.Closed,
+                                                                        true);
+      }
+      Docflow.PublicInitializationFunctions.Module.CreateDocumentTypeExternalLink(Constants.Module.OutgoingInvoiceTypeGuid, Constants.Module.OutgoingInvoiceExternalEntityId);
+    }
   }
 }
